dba6bf6f30a4de263723f20763038811c05a3591
aga134 <aga134w@gmail,com>
2024-12-07 20:36:58 +0300

    final athete_service

---
+++ athlete_handling/app/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Date, UUID
+import uuid
+from database import Base
+
+class Athlete(Base):
+    __tablename__ = 'athletes'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    patronymic = Column(String, index=True)
+    location = Column(String)
+    email = Column(String, index=True)
+    UIN = Column(String, index=True)
+    birth_date = Column(Date)
+    phone_number = Column(String)
